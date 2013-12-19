e032ee41a7dbaf6635432cf5e97c1d71c67581c2
Bikash Sharma <bikash1999@yahoo.com>
2013-12-18 21:58:42 -0800

    Added test

---
+++ tests/mqs-test.js
@@ -0,0 +1,44 @@
+var chai = require("chai"),
+	assert = chai.assert,
+	expect = chai.expect,
+	util = require("util"),
+	restify = require('restify'),
+	Promise = require("promise");
+
+var HOST = "http://127.0.0.1:3000";
+
+var client = restify.createJsonClient({
+	url: HOST
+});
+
+var userObject = {
+	"id": "",
+	"sourceType": "CPM",
+	"timerType": "datetime",
+	"timerStartTime": "",
+	"timerEndTime": "",
+	"cronExpression": "",
+	"callbackurl": "/tqs/testCallback",
+	"data": {
+		"test": "Test the url"
+	},
+	"createDateTime": "",
+	"isProcessed": "",
+	"processedDateTime": ""
+};
+
+
+describe('MQS Test', function() {
+	describe('MQS Test case 1', function() {
+		it('should post a request', function(done) {
+			client.post("/message/user", userObject, function(err, req, res, obj) {
+				if (err) throw (err);
+				expect(res.statusCode).to.equal(200);
+				//expect(obj.id).to.not.equal(null);
+				//newId = obj.id;
+				//console.log("Got an ID: " + newId);				
+				done();
+			});
+		});
+	});
+});
