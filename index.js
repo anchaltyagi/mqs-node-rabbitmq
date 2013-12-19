68f535c543befbb579190c39e5f8e55b4c0d8225
Anchal Tyagi <tyagi.anchal02@gmail.com>
2013-12-18 23:28:14 -0800

    added connection to rabbitMQ

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 var express = require('express');
-var	mqsMQ = require('./lib/mqs-MQ');
+var mqsMQ = require('./lib/mqs-MQ');
 
 
-var config = {	
+var config = {
+	mqsMq: {
+		server: {
+			host: 'localhost',
+			port: 15672,
+			login: 'guest',
+			password: 'guest'
+		},
+		exchangeName: 'mqs-node-rabbitmq',
+		queueName: 'mqs-node-rabbitmq-queue'
+	}
 };
 
 var port = process.env.PORT || 3000;
@@ -11,6 +21,6 @@ var server = app.listen(port);
 console.log('Server listening at %s', port);
 
 //Creating mqs-MQ Server
-mqsMQ.init(config, function(){
+mqsMQ.init(config, function() {
 	mqsMQ.listen(app);
 });
