ecbccfb087d9e8c192009698e68619f191d667e2
Bikash Kumar Sharma <bikash1999@yahoo.com>
2014-11-24 16:20:40 +0530

    made it working

--- index.js
+++ index.js
@@ -3,22 +3,24 @@ var mqsMQ = require('./lib/mqs-MQ');
 
 
 var config = {
-	mqsMq: {
-		server: {
-			host: 'localhost',
-			port: 15672,
-			login: 'guest',
-			password: 'guest'
-		},
-		exchangeName: 'mqs-node-rabbitmq',
-		queueName: 'mqs-node-rabbitmq-queue'
-	}
+  mqsMq: {
+    server: {
+      host: 'localhost',
+      port: 15672,
+      login: 'guest',
+      password: 'guest'
+    },
+    exchangeName: 'mqs-node-rabbitmq',
+    queueName: 'mqs-node-rabbitmq-queue',
+		replyQueueName: 'mqs-node-rabbitmq-queue'
+  }
 };
 
 var port = process.env.PORT || 3000;
 var app = express();
 var server = app.listen(port);
 console.log('Server listening at %s', port);
+
 //Creating mqs-MQ Server
 mqsMQ.init(config, function() {
 	mqsMQ.listen(app);
